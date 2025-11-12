16695f3305cd1754063179368e25c7ebc88a9ddb
pocoloco008 <pacovanderaar@gmail.com>
2025-11-12 15:08:05 +0100

    aap

--- OldvsYoung/Assets/Script/points.cs
+++ OldvsYoung/Assets/Script/points.cs
@@ -1,28 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using JetBrains.Annotations;
+
 public class points : MonoBehaviour
 {
     public static points instance;
 
-
+    [Header("Score UI")]
     public Text jongetje;
     public Text oldman;
+
+    [Header("Reveal Objects (in order of score)")]
+    public GameObject[] revealJongetje; 
+    public GameObject[] revealOldman;   
 
     int scorejongetje = 0;
     int scoreoldman = 0;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         jongetje.text = scorejongetje.ToString();
         oldman.text = scoreoldman.ToString();
-    }
 
+        
+        foreach (GameObject obj in revealJongetje)
+            if (obj != null) obj.SetActive(false);
+
+        foreach (GameObject obj in revealOldman)
+            if (obj != null) obj.SetActive(false);
+    }
 
     private void Update()
     {
@@ -46,15 +53,28 @@ public class points : MonoBehaviour
             SceneManager.LoadScene(2);
         }
     }
+
     public void AddPointsjongetje()
     {
         scorejongetje += 1;
         jongetje.text = scorejongetje.ToString();
+        RevealObjects(revealJongetje, scorejongetje);
     }
 
     public void AddPointsoldman()
     {
         scoreoldman += 1;
         oldman.text = scoreoldman.ToString();
+        RevealObjects(revealOldman, scoreoldman);
+    }
+
+    void RevealObjects(GameObject[] revealList, int score)
+    {
+        int index = score - 1;
+        if (index >= 0 && index < revealList.Length)
+        {
+            if (revealList[index] != null)
+                revealList[index].SetActive(true);
+        }
     }
 }
